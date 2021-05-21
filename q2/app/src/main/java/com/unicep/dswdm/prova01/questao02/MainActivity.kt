efe2fcd7a5ccc948130a40714c4e31431263c1f2
gismarb <gismar_barbosa@yahoo.com.br>
2021-05-20 21:08:36 -0300

    Initial structure

---
+++ q2/app/src/main/java/com/unicep/dswdm/prova01/questao02/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.unicep.dswdm.prova01.questao02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
